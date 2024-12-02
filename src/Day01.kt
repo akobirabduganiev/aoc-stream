ec51d46a3c01faff7281ca8ca8acdcb96d275426
aKO <akobir.abduganiev@ya.ru>
2024-12-02 10:21:39 +0500

    Initial commit

---
+++ src/Day01.kt
@@ -0,0 +1,45 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+import kotlin.math.abs
+
+fun readInput(fileName: String): List<String> = Path("input/$fileName").readLines()
+
+fun main() {
+	
+	part1()
+	part2()
+	
+}
+
+// Day 1 - Part 1
+fun part1() {
+	val lines = readInput("day01.txt")
+	val (left, right) = lines.map { line ->
+		val first = line.substringBefore(" ").toInt()
+		val second = line.substringAfterLast(" ").toInt()
+		first to second
+	}.unzip()
+	
+	val result = left.sorted().zip(right.sorted()).sumOf { (first, second) ->
+		abs(first - second)
+	}
+	println(result)
+}
+
+// Day 1 - Part 2
+fun part2() {
+	val lines = readInput("day01.txt")
+	val (left, right) = lines.map { line ->
+		val first = line.substringBefore(" ").toInt()
+		val second = line.substringAfterLast(" ").toInt()
+		first to second
+	}.unzip()
+	
+	val rightCounts = right.groupingBy { it }.eachCount()
+	
+	val similarityScore = left.sumOf { number ->
+		number * (rightCounts[number] ?: 0)
+	}
+	
+	println(similarityScore)
+}
